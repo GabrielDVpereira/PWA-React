f5d2cb7ebf7e54a9301b726a7fbabc1839cd6553
Gabriel Davi <gdavi.gd@gmail.com>
2020-08-22 00:31:19 -0300

    Add enviroment

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { API_KEY } from "../envVars";
 const URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const fetchWeather = async (query) => {
@@ -7,7 +6,7 @@ export const fetchWeather = async (query) => {
     params: {
       q: query,
       units: "metric",
-      APPID: API_KEY,
+      APPID: process.env.API_KEY,
     },
   });
 
